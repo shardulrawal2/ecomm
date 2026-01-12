1e50a2cacc5f2922cd11d7b0c9bf22c09a8a3e9b
Shardul <shardulrawal2@gmail.com>
2026-01-12 21:03:54 +0530

    Use production backend URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
